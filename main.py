6efc44ced2a9947b5829e1caae76aeea9cc1dad3
K3vin-h <yyc.kevinh@gmail.com>
2024-12-10 08:48:44 -0700

    made the menu a class

--- main.py
+++ main.py
@@ -1,52 +1,67 @@
+class AlbertaHospitalManagement:
+    def __init__(self):
+        print("Welcome to Alberta Hospital (AH) Management System")
+
+    def main_menu(self):
+        while True:
+            menu = input("Select from the following options, or select 3 to stop:\n"
+                         "1 - Doctors\n"
+                         "2 - Patients\n"
+                         "3 - Exit Program\n")
+            if menu == "1":
+                self.doctor_menu()
+            elif menu == "2":
+                self.patient_menu()
+            elif menu == "3":
+                print("Thanks for using the program. Bye!")
+                break
+
+    def doctor_menu(self):
+        from classes.doctorManager import doctorManager
+        doctor_manager = doctorManager()
+        while True:
+            menu = input("\nDoctors Menu:\n"
+                         "1 - Display Doctors list\n"
+                         "2 - Search for doctor by ID\n"
+                         "3 - Search for doctor by name\n"
+                         "4 - Add doctor\n"
+                         "5 - Edit doctor info\n"
+                         "6 - Back to the Main Menu\n")
+            if menu == "1":
+                doctor_manager.display_doctors_list()
+            elif menu == "2":
+                doctor_manager.search_doctor_by_id()
+            elif menu == "3":
+                doctor_manager.search_doctor_by_name()
+            elif menu == "4":
+                doctor_manager.add_dr_to_file()
+            elif menu == "5":
+                doctor_manager.edit_doctor_info()
+            elif menu == "6":
+                break
+
+    def patient_menu(self):
+        from classes.patientManager import PatientManager
+        patient_manager = PatientManager()
+        while True:
+            menu = input("\nPatients Menu:\n"
+                         "1 - Display patients list\n"
+                         "2 - Search for patient by ID\n"
+                         "3 - Add patient\n"
+                         "4 - Edit patient info\n"
+                         "5 - Back to the Main Menu\n")
+            if menu == "1":
+                patient_manager.display_patient_list()
+            elif menu == "2":
+                patient_manager.search_patient_by_id()
+            elif menu == "3":
+                patient_manager.add_patient_to_file()
+            elif menu == "4":
+                patient_manager.edit_patient_info_by_id()
+            elif menu == "5":
+                break
 
 
-def main():
-    print("Welcome to Alberta Hospital (AH) Managment system ")
-    while True:
-        menu = input("Select from the following options, or select 3 to stop:\n1 - 	Doctors\n2 - 	Patients\n3 -	Exit Program\n")
-        if menu == "1":
-            docmenu()
-
-        elif menu == "2":
-            patientmenu()
-
-        elif menu == "3":
-            print("Thanks for using the program. Bye!")
-            break
-
-def docmenu():
-    from classes.doctorManager import doctorManager
-    doctor_manager = doctorManager()
-    while True:
-        menu = input("\nDoctors Menu:\n1 - Display Doctors list\n2 - Search for doctor by ID\n3 - Search for doctor by name\n4 - Add doctor\n5 - Edit doctor info\n6 - Back to the Main Menu\n")
-        if menu == "1":
-            doctor_manager.display_doctors_list()
-        elif menu == "2":
-            doctor_manager.search_doctor_by_id()
-        elif menu == "3":
-            doctor_manager.search_doctor_by_name()
-        elif menu == "4":
-            doctor_manager.add_dr_to_file()
-        elif menu == "5":
-            doctor_manager.edit_doctor_info()
-        elif menu == "6":
-            break
-
-def patientmenu():
-    from classes.patientManager import PatientManager
-    patient_manager = PatientManager()
-    while True:
-        menu = input("\nPatients Menu:\n1 - Display patients list\n2 - Search for patient by ID\n3 - Add patient\n4 - Edit patient info\n5 - Back to the Main Menu\n")
-        if menu == "1":
-            patient_manager.display_patient_list()
-        elif menu == "2":
-            patient_manager.search_patient_by_id()
-        elif menu == "3":
-            patient_manager.add_patient_to_file()
-        elif menu == "4":
-            patient_manager.edit_patient_info_by_id()
-        elif menu == "5":
-            break
-    
 if __name__ == "__main__":
-    main()
+    management = AlbertaHospitalManagement()
+    management.main_menu()
